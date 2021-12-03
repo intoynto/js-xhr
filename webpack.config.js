6a162b49f102832ee37ef03da0d9aa2c4ced39b1
intoynto <intoynto@gmail.com>
2021-12-03 17:08:36 +0800

    update library target

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,60 @@
 const path=require("path");
-const DeclarationBundlerPlugin = require('types-webpack-bundler');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
+const TypescriptDeclarationPlugin = require('typescript-declaration-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const dev=require("./wp.dev");
 
-module.exports={
-    entry:{
-        "xhr":path.resolve(__dirname,"src/index.ts")
-    },
-    output:{
-        path:path.resolve(__dirname,"dist"),
-        chunkFilename: '[name].js',
-        filename: '[name].js'
-    },
-    devtool:'source-map',
-    resolve: { extensions: ['.ts'] },
-    plugins:[
-        new CleanWebpackPlugin(),       
-        new DeclarationBundlerPlugin({
-            moduleName:'"intoy-xhr"',
-            out:'./xhr.d.ts',
-        })        
-    ],
-    module:{
-        rules: [
-            {
-                test: /\.ts$/,
-                exclude: /(node_module|dist)/,
-                use:[
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            cacheDirectory: true,
-                        },
-                    },
-                    {
-                        loader: 'ts-loader',
-                    },
-                    {
-                        loader: '@stavalfi/babel-plugin-module-resolver-loader',
-                        options: {
-                            // all those options will go directly to babel-plugin-module-resolver plugin.
-                            // Read babel-plugin-module-resolver DOCS to see all options:
-                            // https://github.com/tleunen/babel-plugin-module-resolver/blob/master/DOCS.md
-                            root: ['./src'],
-                            extensions: ['.js', '.jsx', '.d.ts', '.ts', '.tsx'],
-                        },
-                    },
-                ],
-            }
-        ]
-    },
-    optimization:{
-        minimizer:[
-            new TerserWebpackPlugin()
+module.exports=function(env,args)
+{
+    const isProd=args && args.mode && args.mode==="production"?true:false;
+    const mode=isProd?"production":"development";
+    console.log("wp.mode ",mode," env ",env);
+
+    const conf=
+    {        
+        output:{
+            path:path.resolve(__dirname,"dist"),
+            chunkFilename: '[name].js',
+            filename: '[name].js',
+            libraryTarget: 'umd',
+            library: `${dev.MODUL_NAME}`,
+            umdNamedDefine: true
+        },
+        devtool:'source-map',
+        resolve: { extensions: ['.ts'] },
+        plugins:[
+            new CleanWebpackPlugin(),       
+            new TypescriptDeclarationPlugin({
+                out:`${dev.MODUL_NAME}.d.ts`,
+            }),       
         ],
-    }
+        module:{
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /(node_module|dist)/,
+                    use:[
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                cacheDirectory: true,
+                            },
+                        },
+                        {
+                            loader: 'ts-loader',
+                        }                       
+                    ],
+                }
+            ]
+        },
+        optimization:{
+            minimizer:[
+                new TerserWebpackPlugin()
+            ],
+        }
+    };
+
+    conf.entry={};
+    conf.entry[`${dev.MODUL_NAME}`]=path.resolve(__dirname,"src/index.ts");
+    return conf;
 };
---
+++ wp.dev.js
@@ -0,0 +1,7 @@
+const MODUL_NAME='xhr';
+const DECLARE_MODUL_NAME="intoy-xhr";
+
+module.exports = {
+    MODUL_NAME,
+    DECLARE_MODUL_NAME,
+}
