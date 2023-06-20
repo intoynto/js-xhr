e9d7d1e8f0bbb19d09469fc7d2ced15c725f153c
Intoy <intoynto@gmail.com>
2023-06-20 09:32:56 +0800

    update serialize params

--- src/serialize.ts
+++ src/serialize.ts
@@ -9,7 +9,16 @@ export function serialize(obj?:any,key?:any,list?:any[]){
         }
     }
     else{
-        list.push(key+='='+encodeURIComponent(obj));
+        if(typeof obj==='string')
+        {
+            if(obj.toString().trim().length>0)
+            {
+                list.push(key+='='+encodeURIComponent(obj));
+            }
+        }
+        else {
+            list.push(key+='='+encodeURIComponent(obj));
+        }
     }
     return list.join('&');
 }
